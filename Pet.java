0f6dcf2111c48259da7913d5d1d2902678e6117f
YuxinRong-10086 <evelynrong11@gmail.com>
2026-06-05 01:06:08 -0700

    Create Pet.java

---
+++ Pet.java
@@ -0,0 +1,63 @@
+import java.io.Serializable;
+
+public abstract class Pet implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private static int totalPets = 0;
+
+        private String name;
+        private int age;
+        private String breed;
+        private int weight;
+
+    public abstract String getSpecies();
+
+    public static int getTotalPets()
+    {
+    return totalPets;
+}
+
+    public void addPet(){
+        totalPets++;
+    }
+
+    public String getName(){
+         return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getBreed(){
+        return breed;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setBreed(String breed){
+        this.breed = breed;
+    }
+
+    public void setWeight(int weight){
+        if(weight <= 0){
+            throw new IllegalArgumentException( "Weight must be greater than 0.");
+    }
+    
+        this.weight = weight;
+    }
+
+    public String toString(){
+        return getSpecies() + ": " + name + ", Age: " + age + ", Breed: " + breed + ", Weight: " + weight;
+    }
+}
